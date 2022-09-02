3f053b0a74b7bf02131b20532bb69d702efb79fc
mi.rodriguez <mi.rodriguez@f5.com>
2022-09-02 15:41:11 -0500

    Added socket io implementation

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(3000, () => {
+messages = []
+app.get('/messages', (req, res) => {
+    res.json(messages);
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+})
+
+io.on('connection', (socket) => {
+    console.log('an user connected');
+})
+
+http.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
